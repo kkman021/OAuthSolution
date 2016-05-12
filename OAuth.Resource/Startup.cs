fd66a5e5d73321d8a17dd5183534d06cd072e7a7
楊明勳 Ming <ming@miniasp.com>
2016-05-12 12:24:32 +0800

    Resource專案加入OAuth啟動類別

---
+++ OAuth.Resource/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OAuth.Resource.Startup))]
+
+namespace OAuth.Resource
+{
+    public  class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseOAuthBearerAuthentication(new Microsoft.Owin.Security.OAuth.OAuthBearerAuthenticationOptions());
+        }
+    }
+}
