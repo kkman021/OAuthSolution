dfc216c9910e97cb2723025db828b7751e0e7465
楊明勳 Ming <ming@miniasp.com>
2016-05-12 11:04:51 +0800

    加入OAuth 啟動類別、加入固定參數專案

---
+++ OAuth.Server/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using OAuth.Server;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace OAuth.Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
