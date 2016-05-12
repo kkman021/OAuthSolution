1a501e242a152324a4676776f58d0086b11455b7
楊明勳 Ming <ming@miniasp.com>
2016-05-12 14:51:10 +0800

    增加用戶端憑證授權專案

---
+++ OAuth.ClientCredentialGrant/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using Constants;
+using DotNetOpenAuth.OAuth2;
+
+namespace OAuth.ClientCredentialGrant
+{
+    internal class Program
+    {
+        private static WebServerClient _webServerClient;
+        private static string _accessToken;
+
+        private static void Main(string[] args)
+        {
+            InitializeWebServerClient();
+
+            Console.WriteLine("要求授權Token...");
+            RequestToken();
+            Console.WriteLine("取得的授權Token: {0}", _accessToken);
+
+            Console.WriteLine("存取受權限控管的WebApi資料...");
+            var data = AccessProtectedResource();
+            Console.WriteLine("會員資料為：{0}", data);
+        }
+
+        private static void InitializeWebServerClient()
+        {
+            var authorizationServerUri = new Uri(Paths.AuthorizationServerBaseAddress);
+            var authorizationServer = new AuthorizationServerDescription
+                                      {
+                                          AuthorizationEndpoint = new Uri(authorizationServerUri, Paths.AuthorizePath),
+                                          TokenEndpoint = new Uri(authorizationServerUri, Paths.TokenPath)
+                                      };
+            _webServerClient = new WebServerClient(authorizationServer, Clients.Client1.Id, Clients.Client1.Secret);
+        }
+
+        private static void RequestToken()
+        {
+            var state = _webServerClient.GetClientAccessToken(new[] {"bio", "notes"});
+            _accessToken = state.AccessToken;
+        }
+
+        private static string AccessProtectedResource()
+        {
+            var resourceServerUri = new Uri(Paths.ResourceServerBaseAddress);
+            var client = new HttpClient(_webServerClient.CreateAuthorizingHandler(_accessToken));
+            return client.GetStringAsync(new Uri(resourceServerUri, Paths.GrabMemberInfo)).Result;
+        }
+    }
+}
