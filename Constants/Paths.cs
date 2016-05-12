9f0c6d0c3476fae52201b8645bd505c7459e691b
楊明勳 Ming <ming@miniasp.com>
2016-05-12 11:49:20 +0800

    增加Client要求授權Controller
    1.增加授權Controller
    2.調整Paths為正確Localhost Port
    3.調整Web.Config允許非SSL連線（上線需事情況調整）

--- Constants/Paths.cs
+++ Constants/Paths.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// AuthorizationServer project should run on this URL 
         /// </summary>
-        public const string AuthorizationServerBaseAddress = "http://localhost:11625";
+        public const string AuthorizationServerBaseAddress = "http://localhost:2552";
 
         /// <summary>
         /// ResourceServer project should run on this URL 
@@ -20,7 +20,7 @@
         /// <summary>
         /// AuthorizationCodeGrant project should be running on this URL. 
         /// </summary>
-        public const string AuthorizeCodeCallBackPath = "http://localhost:38500/";
+        public const string AuthorizeCodeCallBackPath = "http://localhost:3154/";
 
         public const string AuthorizePath = "/OAuth/Authorize";
         public const string TokenPath = "/OAuth/Token";
