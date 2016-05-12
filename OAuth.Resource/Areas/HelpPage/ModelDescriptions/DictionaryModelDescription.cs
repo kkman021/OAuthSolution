e80b2c281bf8293526e38b577aba05e0a6d4d7bc
楊明勳 Ming <ming@miniasp.com>
2016-05-12 12:15:42 +0800

    加入OAuth Resource專案

---
+++ OAuth.Resource/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OAuth.Resource.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OAuth.Resource/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OAuth.Resource.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
