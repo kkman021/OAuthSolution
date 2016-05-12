6f97e69ce845291928491ce88db7a2d4ab3b6795
楊明勳 Ming <ming@miniasp.com>
2016-05-12 15:15:14 +0800

    實做Resource Owner Password Credentials Grant

--- OAuth.ResourceOwnerPasswordCredentialsGrant/Program.cs
+++ OAuth.ResourceOwnerPasswordCredentialsGrant/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using Constants;
+using DotNetOpenAuth.OAuth2;
 
 namespace OAuth.ResourceOwnerPasswordCredentialsGrant
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static WebServerClient _webServerClient;
+        private static string _accessToken;
+
+        private static void Main(string[] args)
         {
+            InitializeWebServerClient();
+
+            Console.WriteLine("要求授權Token...");
+            RequestToken();
+            Console.WriteLine("取得的授權Token: {0}", _accessToken);
+
+            Console.WriteLine("存取受權限控管的WebApi資料...");
+            var data = AccessProtectedResource();
+            Console.WriteLine("會員資料為：{0}", data);
+        }
+
+        private static void InitializeWebServerClient()
+        {
+            var authorizationServerUri = new Uri(Paths.AuthorizationServerBaseAddress);
+            var authorizationServer = new AuthorizationServerDescription
+                                      {
+                                          AuthorizationEndpoint = new Uri(authorizationServerUri, Paths.AuthorizePath),
+                                          TokenEndpoint = new Uri(authorizationServerUri, Paths.TokenPath)
+                                      };
+            _webServerClient = new WebServerClient(authorizationServer, Clients.Client1.Id, Clients.Client1.Secret);
+        }
+
+        private static void RequestToken()
+        {
+            var state = _webServerClient.ExchangeUserCredentialForToken("User Name", "Password",
+                new[] {"bio"});
+            _accessToken = state.AccessToken;
+        }
+
+        private static string AccessProtectedResource()
+        {
+            var resourceServerUri = new Uri(Paths.ResourceServerBaseAddress);
+            var client = new HttpClient(_webServerClient.CreateAuthorizingHandler(_accessToken));
+            return client.GetStringAsync(new Uri(resourceServerUri, Paths.GrabMemberInfo)).Result;
         }
     }
 }
